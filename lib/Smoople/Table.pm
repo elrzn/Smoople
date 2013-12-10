6d6be8e0f5bc59dca67974f10ad80d156383b2a4
Eric Lorenzana <self@elrzn.me>
2013-12-10 13:23:06 +0000

    Perform a search by key 'equals' @params
    Using indexes. Return data for now.

--- lib/Smoople/Table.pm
+++ lib/Smoople/Table.pm
@@ -1,17 +1,31 @@
 package Smoople::Table;
 use Moose;
 use namespace::autoclean;
-use List::Util qw(max);
 
 has 'name',    is => 'ro', isa => 'Str';
 has 'columns', is => 'rw', isa => 'ArrayRef[Smoople::Table::Column]';
 
 sub insert
 { my ($self, %p) = @_;
-  my $last = max map { scalar @{$p{$_}} - 1 } keys %p;
   my @columns = @{$self->columns};
   $_->add(@{$p{$_->name}}) for @columns;
   $self->columns(\@columns);
+}
+
+sub search
+{ my ($self, %p) = @_;
+  my $n_conditions = scalar keys %p;
+  my @indexes      = do
+  { my %h;    # Temporal hash to store candidate indexes.
+    $h{$_}++
+     for map { $_->seek(@{$p{$_->name}}) }
+         map { $self->column($_) }
+         keys %p;
+    grep { $h{$_} == $n_conditions } keys %h;
+  };
+
+  # Just return the data for now.
+  map { [@$_[@indexes]] } map { $_->data } @{$self->columns};
 }
 
 sub column
--- lib/Smoople/Table/Column.pm
+++ lib/Smoople/Table/Column.pm
@@ -12,6 +12,16 @@ sub add
   $self->data(\@data);
 }
 
+sub seek
+{ my ($self, @equals) = @_;
+  my @data    = @{$self->data};
+  my @indexes = ();
+  for my $i (0 .. $#data)
+  { push @indexes, $i if grep { $data[$i] eq $_ } @equals;
+  }
+  @indexes;
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
