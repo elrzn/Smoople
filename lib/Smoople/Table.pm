a705b97f1e93ca7cec606f25532cfbadbcb601d0
Eric Lorenzana <self@elrzn.me>
2013-12-10 12:14:54 +0000

    Insert using a column approach rather than a record one

--- lib/Smoople/Table.pm
+++ lib/Smoople/Table.pm
@@ -10,11 +10,7 @@ sub insert
 { my ($self, %p) = @_;
   my $last = max map { scalar @{$p{$_}} - 1 } keys %p;
   my @columns = @{$self->columns};
-  for my $index (0 .. $last)
-  { my %params = map { $_ => $p{$_}->[$index] } keys %p;
-    my $record = Smoople::Table::Record->new(data => \%params);
-    $_->add($record->data->{$_->name}) for @columns;
-  }
+  $_->add(@{$p{$_->name}}) for @columns;
   $self->columns(\@columns);
 }
 
